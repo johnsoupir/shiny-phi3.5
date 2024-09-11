342530d58565293c77a208601008928965e71312
John Soupir <50882613+johnsoupir@users.noreply.github.com>
2024-09-11 12:30:46 -0500

    Updates fine tuning script

--- train-shiny-phi3.5.py
+++ train-shiny-phi3.5.py
@@ -1,30 +1,23 @@
-from unsloth import FastLanguageModel 
-from unsloth import is_bfloat16_supported
+from unsloth import FastLanguageModel
 import torch
-from trl import SFTTrainer
-from transformers import TrainingArguments
-from datasets import load_dataset
-max_seq_length = 2048 # Supports RoPE Scaling internally
+max_seq_length = 2048 #Ctx length 
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True #Use 4-bit quant.
 
-
-### Model Properties
-data_file = "Datasets/formatted_reflection_v2.jsonl"
-new_model_name = "shiny-phi3.5"
-dataset_url = data_file #Can also be huggingface address for online dataset
-
-dataset = load_dataset("json", data_files = {"train" : dataset_url}, split = "train")
+output_model_name = "Shiny-Phi3.5" #Name of final model
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Phi-3.5-mini-instruct-bnb-4bit",
+    model_name = "unsloth/Phi-3.5-mini-instruct",
     max_seq_length = max_seq_length,
-    dtype = None,
-    load_in_4bit = True,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
 )
 
-#Model patching and add fast LoRA weights
+#Add LoRA adapters
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 16,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",],
     lora_alpha = 16,
@@ -32,33 +25,63 @@ model = FastLanguageModel.get_peft_model(
     bias = "none",    # Supports any, but = "none" is optimized
     use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
     random_state = 3407,
-    max_seq_length = max_seq_length,
     use_rslora = False,  # We support rank stabilized LoRA
     loftq_config = None, # And LoftQ
 )
 
+# Example of the desired chat format
+"""
+<|user|>
+Hi!<|end|>
+<|assistant|>
+Hello! How are you?<|end|>
+<|user|>
+I'm doing great! And you?<|end|>
+"""
+
+from unsloth.chat_templates import get_chat_template
+
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "phi-3", # Supports zephyr, chatml, mistral, llama, alpaca, vicuna, vicuna_old, unsloth
+    mapping = {"role" : "from", "content" : "value", "user" : "human", "assistant" : "gpt"}, # ShareGPT style
+)
+
+def formatting_prompts_func(examples):
+    convos = examples["conversations"]
+    texts = [tokenizer.apply_chat_template(convo, tokenize = False, add_generation_prompt = False) for convo in convos]
+    return { "text" : texts, }
+pass
+
+from datasets import load_dataset
+dataset = load_dataset("mahiatlinux/Reflection-Dataset-ShareGPT-v2", split = "train")
+dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+"""Let's see how the `Phi-3` format works by printing the 5th element"""
+
+dataset[5]["conversations"]
+
+print(dataset[5]["text"])
+
+
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+
 trainer = SFTTrainer(
     model = model,
+    tokenizer = tokenizer,
     train_dataset = dataset,
     dataset_text_field = "text",
     max_seq_length = max_seq_length,
-    tokenizer = tokenizer,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
     args = TrainingArguments(
-        # per_device_train_batch_size = 2,
-        # gradient_accumulation_steps = 4,
-        # warmup_steps = 10,
-        # max_steps = 60,
-        # fp16 = not is_bfloat16_supported(),
-        # bf16 = is_bfloat16_supported(),
-        # logging_steps = 1,
-        # output_dir = "outputs",
-        # optim = "adamw_8bit",
-        # seed = 3407,
         per_device_train_batch_size = 2,
         gradient_accumulation_steps = 4,
         warmup_steps = 5,
-        # max_steps = 60,
         num_train_epochs = 1,
+        # max_steps = 60,
         learning_rate = 2e-4,
         fp16 = not is_bfloat16_supported(),
         bf16 = is_bfloat16_supported(),
@@ -70,10 +93,78 @@ trainer = SFTTrainer(
         output_dir = "outputs",
     ),
 )
-trainer.train()
 
-# Export model to GGUF for llama.cpp
-model.save_pretrained_gguf(new_model_name, tokenizer, quantization_method = "q8_0")
-model.save_pretrained_gguf(new_model_name, tokenizer, quantization_method = "q4_0")
-model.save_pretrained_gguf(new_model_name, tokenizer, quantization_method = "q4_K_M")
+#Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
 
+trainer_stats = trainer.train()
+
+#Show final memory and time stats
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory         /max_memory*100, 3)
+lora_percentage = round(used_memory_for_lora/max_memory*100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training.")
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+#Test the newly trained model
+from unsloth.chat_templates import get_chat_template
+
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "phi-3", # Supports zephyr, chatml, mistral, llama, alpaca, vicuna, vicuna_old, unsloth
+    mapping = {"role" : "from", "content" : "value", "user" : "human", "assistant" : "gpt"}, # ShareGPT style
+)
+
+FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+
+messages = [
+    {"from": "human", "value": "Continue the fibonnaci sequence: 1, 1, 2, 3, 5, 8,"},
+]
+inputs = tokenizer.apply_chat_template(
+    messages,
+    tokenize = True,
+    add_generation_prompt = True, # Must add for generation
+    return_tensors = "pt",
+).to("cuda")
+
+from transformers import TextStreamer
+text_streamer = TextStreamer(tokenizer, skip_prompt = True)
+_ = model.generate(input_ids = inputs, streamer = text_streamer, max_new_tokens = 128, use_cache = True)
+
+#Save the results:
+
+#First we'll save the LoRA
+model.save_pretrained(output_model_name + "_lora_model") # Local saving
+tokenizer.save_pretrained(output_model_name + "_lora_model")
+# model.push_to_hub("your_name/lora_model", token = "...") # Online saving
+# tokenizer.push_to_hub("your_name/lora_model", token = "...") # Online saving
+
+# Now we'll merge the LoRA into the model 
+
+# Merge to 16bit
+if True: model.save_pretrained_merged(output_model_name + "_safetensors-model", tokenizer, save_method = "merged_16bit",)
+if False: model.push_to_hub_merged("hf/model", tokenizer, save_method = "merged_16bit", token = "")
+
+# Merge to 4bit
+if False: model.save_pretrained_merged("model", tokenizer, save_method = "merged_4bit",)
+if False: model.push_to_hub_merged("hf/model", tokenizer, save_method = "merged_4bit", token = "")
+
+# Just LoRA adapters
+if True: model.save_pretrained_merged("lora-model", tokenizer, save_method = "lora",)
+if False: model.push_to_hub_merged("hf/model", tokenizer, save_method = "lora", token = "")
+
+# Now use Llama.cpp to convert to GGUF and quantize. 
+# Built in unsloth conversion produces broken GGUFs, so use Llama.cpp directly. 
+
+print("########################################") 
+print("TRAINING COMPLETE!") 
+print("########################################") 
